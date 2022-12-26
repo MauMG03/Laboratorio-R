60e9fb288137d0364c540c588c549627e2571400
ITS - Mauricio Munoz <mauricio.munoz@its.com.co>
2022-12-26 17:20:47 -0500

    importar datos del excel

--- Script.R
+++ Script.R
@@ -8,10 +8,16 @@
 
 # Instalacion de paquetes
 #install.packages("easypackages")
+#install.packages("readxl")
 
 library("easypackages")
+library("readxl")
 
 lib_req <- c("lubridate","dplyr","visdat","missMDA","mice","DMwR2","editrules","corrplot")
 easypackages::packages(lib_req)
 
-  
+#Importar datos del excel
+
+Datos <- read_excel(".\\paises.xls")
+ 
+
