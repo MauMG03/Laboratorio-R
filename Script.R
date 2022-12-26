68f21575675cdaa33bc1a9eea5b449f0de6af28e
ITS - Mauricio Munoz <mauricio.munoz@its.com.co>
2022-12-26 16:31:17 -0500

    añadir script y librerias

---
+++ Script.R
@@ -0,0 +1,17 @@
+#------------------------------------------------------#
+#--- Probabilidad y Estadistica       Grupo:2       ---#
+#--- Laboratorio en R                               ---#
+#--- Integrantes:                                   ---#
+#--- Mauricio Munoz Gutierrez - 2123687             ---#
+#--- Juan Sebastian Getial Getial - XXXXXXX         ---#
+#------------------------------------------------------#
+
+# Instalacion de paquetes
+#install.packages("easypackages")
+
+library("easypackages")
+
+lib_req <- c("lubridate","dplyr","visdat","missMDA","mice","DMwR2","editrules","corrplot")
+easypackages::packages(lib_req)
+
+  
