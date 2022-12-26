2d0d54f12ddb216a55ee389f6218233b15e1766a
ITS - Mauricio Munoz <mauricio.munoz@its.com.co>
2022-12-26 17:54:29 -0500

    Prepocesamiento de GRUPOS

--- Script.R
+++ Script.R
@@ -19,5 +19,37 @@ easypackages::packages(lib_req)
 #Importar datos del excel
 
 Datos <- read_excel(".\\paises.xls")
- 
+
+str(Datos)
+summary(Datos) 
+
+##--------------------- PREPROCESAMIENTO DE DATOS ---------------------##
+
+#Observando las etiquetas de la variable GRUPOS
+table(Datos$GRUPOS)
+
+#Declaracion de niveles correctos para las variables tipo Factor.
+level_GRUPOS <- c(africa="AFRICA",
+                  Africa="AFRICA",
+                  AFRICA="AFRICA",
+                  asia="ASIA",
+                  Asia="ASIA",
+                  ASIA="ASIA",
+                  'EO-NA_JAPON_AUSTR_NZ'="EO-NA_JAPON_AUSTR_NZ",
+                  'Europa Oriental'="EUROPA ORIENTAL",
+                  EUROPA_ORIENTAL="EUROPA ORIENTAL",
+                  iberoamerica="IBEROAMERICA",
+                  Iberoamerica="IBEROAMERICA",
+                  IBEROAMERICA="IBEROAMERICA",
+                  ORIENTE_MEDIO="ORIENTE MEDIO")
+
+#Modificacion del formato y transformacion de variables
+
+Datos <- transform(Datos,
+                   GRUPOS= factor(dplyr::recode(GRUPOS, !!!level_GRUPOS))
+                   )
+
+str(Datos$GRUPOS)
+summary(Datos)
+
 
